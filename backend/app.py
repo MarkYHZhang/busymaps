7d32a2ecab3a98620bee466b2f24b788ffee3e44
Mark Zhang <markzhang.inbox@gmail.com>
2020-08-22 18:49:36 -0400

    Backend deploy + frontend index relocation

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,105 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import json
 import populartimes
 from flask import jsonify
 from flask import request
-app = Flask(__name__)
+from flask_cors import CORS
+
+app = Flask(__name__, static_url_path='')
+CORS(app, support_credentials=True)
+
+idling = None
+with open('backend/idling.json') as f:
+    idling = json.load(f)
+
+busy = []
+with open('backend/parks.json') as f:
+    busy += json.load(f)
+
+from flask import render_template
+@app.route('/', methods=['GET'])
+def metrics():
+    return render_template("frontend/index/index.html")
+
 
 """
-sLongitude, sLatitude, eLongitude, sLatitude
+swLongitude, swLatitude, neLongitude, neLatitude, hourOfDay
 """
-@app.route("/heatmap/", methods = ['POST'])
-def heatmap():
-    # print(populartimes.get_id("AIzaSyDf_VkuxitxTpHvue5pxD4COY92PUMqrIM", "ChIJSYuuSx9awokRyrrOFTGg0GY"))
-    # populartimes.get("AIzaSyDf_VkuxitxTpHvue5pxD4COY92PUMqrIM", [], (48.132986, 11.566126), (48.142199, 11.580047))
-    data = request.json
-    print(data['sLongitude'])
-    return "lol"
+@app.route("/traffic/", methods = ['POST'])
+def traffic():
+    data = json.loads(request.data)
+    swLatitude = float(data['swLatitude'])
+    swLongitude = float(data['swLongitude'])
+    neLatitude = float(data['neLatitude'])
+    neLongitude = float(data['neLongitude'])
+    hourOfDay = str(data["hourOfDay"]).zfill(2)
+    response = []
+    for place in idling:
+        lng = float(place["Longitude"])
+        lat = float(place["Latitude"])
+        if not (swLatitude <= lat <= neLatitude and swLongitude <= lng <= neLongitude):
+            continue
+        rawVal = str(place["IdlingByHour"])
+        idlingByHour = json.loads(rawVal)[str(hourOfDay)]
+        response.append({
+            "latitude": lat,
+            "longitude": lng,
+            "percentage": idlingByHour
+        })
+    return jsonify(response)
 
+
+@app.route("/placebusyness/", methods=['POST'])
+def placebusyness():
+    data = json.loads(request.data)
+    place_id = data['placeID']
+    dayOfWeek = data['dayOfWeek']  # first capitalized day of week
+    place = populartimes.get_id("AIzaSyC5xsFHLdEzj_3Ce8l8A2eqxDB0EvAvyQ8", place_id)
+    if "populartimes" not in place:
+        return jsonify({"response": "NOT_FOUND"})
+    val = []
+    for day in place["populartimes"]:
+        if (day["name"] == dayOfWeek):
+            val = day["data"]
+            break
+    lat = float(place["coordinates"]["lat"])
+    lng = float(place["coordinates"]["lng"])
+    response = {
+        "latitude": lat,
+        "longitude": lng,
+        "percentage": val
+    }
+    return jsonify(response)
+
+
+"""
+swLongitude, swLatitude, neLongitude, neLatitude, hourOfDay
+"""
+@app.route("/busyness/", methods = ['POST'])
+def busyness():
+    data = json.loads(request.data)
+    swLatitude = float(data['swLatitude'])
+    swLongitude = float(data['swLongitude'])
+    neLatitude = float(data['neLatitude'])
+    neLongitude = float(data['neLongitude'])
+    hourOfDay = int(data["hourOfDay"])
+    response = []
+    for place in busy:
+        lat = float(place["coordinates"]["lat"])
+        lng = float(place["coordinates"]["lng"])
+        if not (swLatitude <= lat <= neLatitude and swLongitude <= lng <= neLongitude):
+            continue
+        hourBusy = 0
+        for day in place["populartimes"]:
+            d = day["data"]
+            hourBusy += d[hourOfDay]
+        hourBusy /= 7.0 * 100
+        response.append({
+            "latitude": lat,
+            "longitude": lng,
+            "percentage": round(hourBusy*100000)/100000
+        })
+    return jsonify(response)
 
 
 if __name__ == '__main__':
